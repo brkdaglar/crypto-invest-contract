08a0e67f90232320dc435821bc776052d82cbfd0
brkdglr <dagburak123@gmail.com>
2022-08-11 17:15:42 +0300

    hardhat config v2

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const MNEMONIC_KEY = process.env.MNEMONIC_KEY;
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
+  // Default Network
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+    hardhat: {
+      chainId: 1337,
+      accounts: {
+        mnemonic: MNEMONIC_KEY,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   solidity: "0.8.10",
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,37 @@
 // imports
+const { ethers, run, network } = require("hardhat");
 
 // async main
-async function main() {}
+async function main() {
+  const LegacyContract = await ethers.getContractFactory("LegacyContract");
+  console.log("Deploying contract...");
+  const legacy = await LegacyContract.deploy("Burak");
+  await legacy.deployed();
+  console.log("Deployed");
+  console.log(`Deployed contract to: ${legacy.address}`);
+  console.log(network.config);
+}
+// verify
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArgument: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already verfied");
+    } else {
+      console.log(e);
+    }
+  }
+}
 
 // main
-main().then(() => process.exit(0));
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
